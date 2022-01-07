f7ebb20e69e34e0f61bfc797f0e467ce37847195
Dipesh Marathe <76129013+dipesh9765@users.noreply.github.com>
2022-01-07 17:25:04 +0530

    Create ceilOfNum.java

---
+++ ceilOfNum.java
@@ -0,0 +1,22 @@
+  public class CeilingArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 5, 9, 14, 16, 19, 24};
+        int ceiled = 3;
+        int start = 0;
+        Math.ceil(2);
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > ceiled) {
+                end = mid - 1;
+            } else if (arr[mid] < ceiled) {
+                start = mid + 1;
+            } else if (arr[mid] == ceiled) {
+                System.out.println("ceiled num :  " + arr[mid]);
+                System.exit(1);
+            }
+        }
+        System.out.println("ceiled num is : " + arr[start]);
+    }
+}
